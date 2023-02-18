3c80a98222d1591586cb85f05eabf39f6e4f5e65
HakaiAlex <akuev.sasha@mail.ru>
2023-02-18 21:43:56 +0300

    -_-

---
+++ Decryption/Decryption/Decryption/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Decryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(287, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Тут можно зашифровать сообщение";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(195, 86);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(456, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(195, 192);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(456, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(241, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "--";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(122, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Шифр:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(122, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(338, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите сообщение, используя кириллицу:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(122, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(343, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Введите слово-ключ, используя кириллицу:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(345, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(195, 388);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(456, 29);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(122, 362);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(262, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Слово - ключ, для расшифровки:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(122, 498);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(273, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Ваша зашифрованное сообщение:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(345, 438);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 38);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(191, 543);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(19, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "--";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 614);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Decryption/Decryption/Decryption/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decryption
+{
+    public partial class Form1 : Form
+    {
+        public static string message;
+        public static string key;
+        public static string cipher;
+        public enum RussianAlphabet
+        {
+            А = 1,
+            Б,
+            В,
+            Г,
+            Д,
+            Е,
+            Ё,
+            Ж,
+            З,
+            И,
+            Й,
+            К,
+            Л,
+            М,
+            Н,
+            О,
+            П,
+            Р,
+            С,
+            Т,
+            У,
+            Ф,
+            Х,
+            Ц,
+            Ч,
+            Ш,
+            Щ,
+            Ъ,
+            Ы,
+            Ь,
+            Э,
+            Ю,
+            Я
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            message = textBox1.Text;
+            key = textBox2.Text;
+            cipher = string.Join(" ", EncryptMessage(message, key));
+            label2.Text = cipher;
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == key)
+                label8.Text = DecipherMessege(cipher, textBox3.Text);
+            else
+                label8.Text = "Неверно введен ключ";
+            textBox3.Text = "";
+        }
+
+        public static string EncryptMessage(string message, String key)
+        {
+            int[] messangeInNumbers = ConvertToNumeric(message);
+            int[] keyInNimbers = ConvertToNumeric(key);
+            int[] result = new int[messangeInNumbers.Length];
+            string text;
+            for (int i = 0, j = 0; i < messangeInNumbers.Length; i++, j++)
+            {
+                if (j == keyInNimbers.Length)
+                    j = 0;
+                result[i] = messangeInNumbers[i] + keyInNimbers[j];
+            }
+
+            return text = string.Join(" ", result);
+        }
+
+
+        /// <summary>
+        /// Расшифровать числа
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string DecipherMessege(string message, string key)
+        {
+            int[] messageInNumbers = ConvertToPrimeArray(message);
+            int[] keyInNimbers = ConvertToNumeric(key);
+            int[] result = new int[messageInNumbers.Length];
+            string text;
+            for (int i = 0, j = 0; i < messageInNumbers.Length; i++, j++)
+            {
+                if (j == keyInNimbers.Length)
+                    j = 0;
+                result[i] = messageInNumbers[i] - keyInNimbers[j];
+            }
+            return ConvertToString(result);
+
+
+        }
+        /// <summary>
+        /// Переводит строку в массив чисел
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int[] ConvertToNumeric(string input)
+        {
+            // Приводим входную строку к верхнему регистру, чтобы игнорировать разницу в регистрах
+            input = RemoveExtraSpaces(input).ToUpper();
+
+            // Создаем массив для хранения результата
+            int[] result = new int[input.Length];
+
+            // Проходим по каждому символу входной строки
+            for (int i = 0; i < input.Length; i++)
+            {
+                // Получаем соответствующее значение для символа на основе перечисления EnglishAlphabet
+                if (Enum.TryParse<RussianAlphabet>(input[i].ToString(), out RussianAlphabet letter))
+                {
+                    result[i] = (int)letter;
+                }
+                else if (input[i] == ' ')
+                    result[i] = 0;
+                else
+                {
+                    // Если символ не является буквой русского алфавита, то устанавливаем значение -1
+                    // (можно выбрать и другое значение, которое будет означать "неизвестный символ")
+                    result[i] = -1;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Конвертируем числовой массив в строку
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int[] ConvertToPrimeArray(string input)
+        {
+            var numbers = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var primes = new List<int>();
+
+            foreach (var numberString in numbers)
+            {
+                if (int.TryParse(numberString, out int number))
+                {
+                    primes.Add(number);
+                }
+            }
+
+            return primes.ToArray();
+        }
+
+        /// <summary>
+        /// Конвертирует массив чисел в строку
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string ConvertToString(int[] input)
+        {
+            // Создаем строку для последующего добавления символов
+            StringBuilder sb = new StringBuilder();
+            //Перебираем массив
+            foreach (int number in input)
+            {
+                //Если 0 то это пробел 
+                if (number == 0)
+                {
+                    sb.Append(' ');
+                }
+                //Если -1 то это неизвестный нам символ
+                else if (number == -1)
+                {
+                    sb.Append('?');
+                }
+                else
+                {
+                    sb.Append(Enum.GetName(typeof(RussianAlphabet), number));
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Удаляет лишние пробелы и символы переноса строки
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string RemoveExtraSpaces(string input)
+        {
+            // Удаление лишних пробелов
+            string output = string.Join(" ", input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            // Удаление символов переноса строки
+            output = output.Replace("\r", string.Empty);
+            output = output.Replace("\n", string.Empty);
+
+            return output;
+        }
+
+       
+    }
+}
